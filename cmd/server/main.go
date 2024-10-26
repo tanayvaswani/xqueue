e1a57b332ba216f4493353c9c6370ee160cbff55
tanayvaswani <vaswani.tanay9@gmail.com>
2024-10-26 21:16:10 +0530

    feat: Test function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,18 @@
 package server
+
+import (
+	"context"
+
+	"github.com/tanayvaswani/xqueue/pb"
+)
+
+type Server struct {
+	pb.UnimplementedTaskServer
+}
+
+func (s *Server) CreateTask(
+	ctx context.Context,
+	in *pb.CreateTaskRequest,
+) (*pb.CreateTaskResponse, error) {
+	return &pb.CreateTaskResponse{TaskId: "1"}, nil
+}
